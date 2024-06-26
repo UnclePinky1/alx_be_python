e918ba10227300c9d84284c776dd5516277530f1
unknown <lerato03khumalo@gmail.com>
2024-06-26 13:21:41 +0200

    complete work

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+P = float(input('principal amount: '))
+R = float (input("rate of interest: "))
+T = int (input("for the time period: "))
+
+Simple_interest = (P*R*T)/100
+print('simple_interest', Simple_interest)
