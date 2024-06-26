e6eddbadf7bce94d288e43321c0b141f9c613a5b
unknown <lerato03khumalo@gmail.com>
2024-06-26 12:27:28 +0200

    school work

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5 
+add = (number1 + number2)
+sub = (number1 - number2)
+mult = (number1 * number2)
+print('Addition of 10 and 5 is', add)
+print('Difference of 10 and 5 is', sub)
+print('Product of 10 and 5 is', mult)
